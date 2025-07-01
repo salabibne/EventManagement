b74f7256bd7581a36ace6a8261173a277c243991
salabibne <salabyaham@gmail.com>
2025-07-01 12:04:17 +0600

    server config and port chnages

--- server.js
+++ server.js
@@ -13,5 +13,7 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/events', require('./routes/eventRoutes'));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0', () =>
+  console.log(`✅ Server running on http://0.0.0.0:${PORT}`)
+);
